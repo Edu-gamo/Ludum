7d23290801e8353e9509e80039c6a27d39257ba6
Edu-gamo <eduard_gm94@hotmail.com>
2018-12-02 16:51:45 +0100

    Main menu funcional, falta hacer pantalla opciones y creditos

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void LoadScene(int index) {
+        SceneManager.LoadScene(index);
+    }
+
+    public void ExitGame() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
