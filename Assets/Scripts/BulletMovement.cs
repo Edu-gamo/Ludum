1c9ebe97546ec0b67b02008593cc9b9dcb422e01
Edu-gamo <eduard_gm94@hotmail.com>
2018-12-03 19:10:52 +0100

    HUD final, fabrica elfos muestra cantidad de elfos trabajando

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -11,8 +11,8 @@ public class BulletMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/ElfHouseManager.cs
+++ Assets/Scripts/ElfHouseManager.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ElfHouseManager : MonoBehaviour {
 
     public int elfsWorking = 0;
+    public int elfsNeeded = 10;
 
     private float workCounter = 0;
     public float maxWorkTime = 30;
     public float minWorkTime = 10;
     public int presentIncrement = 1;
 
+    private Text elfsText;
+
     // Use this for initialization
     void Start () {
-		
-	}
+
+        elfsText = GetComponentInChildren<Text>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,14 +37,16 @@ public class ElfHouseManager : MonoBehaviour {
             }
 
         }
-		
-	}
+
+        elfsText.text = elfsWorking.ToString();
+
+    }
 
     public bool AddElf() {
 
         if (maxWorkTime > minWorkTime) {
             elfsWorking++;
-            if (elfsWorking % 5 == 0) maxWorkTime--;
+            if (elfsWorking % elfsNeeded == 0) maxWorkTime--;
             return true;
         }
         return false;
